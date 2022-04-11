19e589f4163d3f2e75215532a0afd255475f9426
PaulNelsonOld <90969624+comcel1@users.noreply.github.com>
2022-04-11 15:38:57 -0500

    added comment functionality

--- server.js
+++ server.js
@@ -8,18 +8,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.use(require('./routes'));
-
 // This tells Mongoose which database we want to connect to.
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true);
+
+app.use(require('./routes'));
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
