73cf6fe9f7a9409d76f61c44698f2dada9ccf02b
PaulNelsonOld <90969624+comcel1@users.noreply.github.com>
2022-04-14 12:50:30 -0500

    finished reply functionality

--- models/Comment.js
+++ models/Comment.js
@@ -1,35 +1,9 @@
 const { Schema, model, Types } = require('mongoose');
-
-const CommentSchema = new Schema(
-  {
-    writtenBy: {
-      type: String,
-    },
-    commentBody: {
-      type: String,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-    },
-    replies: [ReplySchema],
-  },
-  {
-    toJSON: {
-      virtuals: true,
-      getters: true,
-    },
-    id: false,
-  }
-);
-
-CommentSchema.virtual('replyCount').get(function () {
-  return this.replies.length;
-});
+const dateFormat = require('../utils/dateFormat');
 
 const ReplySchema = new Schema(
   {
-    // set custom id to avoid confusion with parent comment's _id field
+    // set custom id to avoid confusion with parent comment _id
     replyId: {
       type: Schema.Types.ObjectId,
       default: () => new Types.ObjectId(),
@@ -52,6 +26,35 @@ const ReplySchema = new Schema(
     },
   }
 );
+
+const CommentSchema = new Schema(
+  {
+    writtenBy: {
+      type: String,
+    },
+    commentBody: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    // use ReplySchema to validate data for a reply
+    replies: [ReplySchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+CommentSchema.virtual('replyCount').get(function () {
+  return this.replies.length;
+});
 
 const Comment = model('Comment', CommentSchema);
 
--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const {
   addComment,
   removeComment,
+  addReply,
+  removeReply,
 } = require('../../controllers/comment-controller');
 
 // /api/comments/<pizzaId>
@@ -9,6 +11,9 @@ router.route('/:pizzaId').post(addComment);
 
 // /api/comments/<pizzaId>/<commentId>
 // the removeComment method is a DELETE callback
-router.route('/:pizzaId/:commentId').delete(removeComment);
+// Remember that the callback function of a route method has req and res as parameters, so we don't have to explicitly pass any arguments to addReply.
+router.route('/:pizzaId/:commentId').put(addReply).delete(removeComment);
+
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
 
 module.exports = router;
